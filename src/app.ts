dd3568c47a56665e87feac689a173e15b7387b30
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-01-06 11:48:34 -0500

    housekeeping: create controllers folder for healthCheckController

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { englishGrammarController } from './about-me/controllers/languages';
+import { healthCheckController } from './shared/controllers';
 
 dotenv.config();
 
@@ -13,8 +14,6 @@ app.use(express.json());
 app.post('/api/languages/english/grammar', englishGrammarController);
 
 // Health check
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ status: 'XForce Serverless API is running' });
-});
+app.get('/', healthCheckController);
 
 export default app;
