ced1fb4417c6d602461be93873743ca2bb9c018b
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-01-06 11:50:30 -0500

    housekeeping: create controllers folder for healthCheckController part 2

--- src/shared/controllers/index.ts
+++ src/shared/controllers/index.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 
 export const healthCheckController = (req: Request, res: Response) => {
-  res.status(200).json({ status: 'XForce Serverless API is running' });
+  res.status(200).json({
+    status: 'XForce Serverless API is running',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime()
+  });
 };
 
